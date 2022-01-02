cddccc755adb30570c5ae05dcfc4e847b2a877b8
Kelli <kellistone217@gmail.com>
2022-01-01 19:08:03 -0700

    Added On blur functionality to name input field

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const containerDiv = document.getElementById("container")
 
 const heading = document.getElementById("heading")
-heading.addEventListener("mouseenter", function( event ) {
+heading.addEventListener("mouseenter", function (event) {
     // highlight the mouseenter target
     event.target.style.color = "purple";
-  
+
     // reset the color after a short delay
-    setTimeout(function() {
-      event.target.style.color = "grey";
+    setTimeout(function () {
+        event.target.style.color = "grey";
     }, 500);
-  }, false);
+}, false);
 
 const bodyElement = document.body;
 bodyElement.style.backgroundColor = "white"
@@ -22,7 +22,7 @@ containerDiv.appendChild(paragraphElement)
 
 
 const paragraphElement2 = document.createElement("p")
-paragraphElement2.textContent = "With gusto. One hundred dollars. No! The cat shelter's on to me. Oh no! The professor will hit me! But if Zoidberg 'fixes' it… then perhaps gifts! I've been there. My folks were always on me to groom myself and wear underpants. What am I, the pope? Leela's gonna kill me. There, now he's trapped in a book I wrote: a crummy world of plot holes and spelling errors! Who am I making this out to? Can I use the gun? Bender, quit destroying the universe!" 
+paragraphElement2.textContent = "With gusto. One hundred dollars. No! The cat shelter's on to me. Oh no! The professor will hit me! But if Zoidberg 'fixes' it… then perhaps gifts! I've been there. My folks were always on me to groom myself and wear underpants. What am I, the pope? Leela's gonna kill me. There, now he's trapped in a book I wrote: a crummy world of plot holes and spelling errors! Who am I making this out to? Can I use the gun? Bender, quit destroying the universe!"
 paragraphElement2.style.color = "black"
 paragraphElement2.style.fontWeight = "bold"
 containerDiv.appendChild(paragraphElement2)
@@ -43,8 +43,20 @@ myButton2.textContent = "Hawkins"
 containerDiv.appendChild(myButton2)
 
 myButton2.addEventListener("click", () => {
-    paragraphElement2.style.color = "green"
-    paragraphElement.style.color = "green"
-    bodyElement.style.backgroundColor = "#ffd494"
+    paragraphElement2.style.color = "white"
+    paragraphElement.style.color = "white"
+    bodyElement.style.backgroundColor = "blue"
 })
+
+let inputField = document.getElementById("demo")
+document.getElementById("demo").addEventListener("keypress", () => { 
+    inputField.style.backgroundColor = "red"
+})
+
+let nameField = document.getElementById("namefield")
+
+function myFunction() {
+    let x = document.getElementById("namefield");
+    x.value = x.value.toUpperCase();
+  }
 
